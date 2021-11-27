4491e23e661670f5bb8bd40a6df72179dcf2a208
Yamaguchi Hiromitsu <yamagame@mac.com>
2021-11-28 01:52:33 +0900

    コマンドボタン修正

--- public/csv-index.js
+++ public/csv-index.js
@@ -1,12 +1,33 @@
 let controller;
 
 function main() {
+  function request(method, url, body, callback) {
+    const XHR = new XMLHttpRequest();
+    XHR.addEventListener("load", function (event) {
+      const response = () => {
+        try {
+          return JSON.parse(XHR.response);
+        } catch {
+          //
+        }
+        return XHR.response;
+      };
+      if (callback) callback(response());
+    });
+    XHR.addEventListener("error", function (event) {
+      console.error(event);
+    });
+    XHR.open(method, url);
+    XHR.setRequestHeader("content-type", "application/json");
+    XHR.send(JSON.stringify(body));
+  }
+
   controller = CsvTable(
     "/csv/view",
     "csv-table",
     ".csv-data-input",
     (file, text) => {
-      postRequest("/csv/command", { file, text }, res => {
+      request("post", "/csv/command", { file, text }, res => {
         console.log(res);
       });
     }
